698801091e1ffd30a1b2c953fff187b512a050af
Anandakrishnan <anandakrishnanayyappan@gmail.com>
2021-06-03 20:50:17 +0530

    update code

--- src/App.js
+++ src/App.js
@@ -1,69 +1,62 @@
 import './App.css'
 import { useState } from 'react'
+import Alert from 'react-bootstrap/Alert'
 
 function App() {
-  const [toDOs, settoDOs] = useState([])
-  const [toDO, settoDO] = useState('')
+  let date = new Date()
+  let options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+
+  const [Todo, setTodo] = useState([])
+  const [todo, settodo] = useState('')
+
   return (
     <div className='app'>
-      <div className='mainHeading'>
-        <h1>ToDo List</h1>
+      <div className='title'>
+        <h1>KuDu toDO</h1>
       </div>
-      <div className='subHeading'>
-        <br />
-        <h2> Wednesday 🌝 ☕ </h2>
+      <div className='date'>
+        <h2>{date.toLocaleString('en-US', options)}</h2>
       </div>
-      <div className='input'>
+      <div className='container'>
         <input
+          value={todo}
+          style={{ color: 'black', width: '250px' }}
           type='text'
-          value={toDO}
-          onChange={(e) => settoDO(e.target.value)}
-          placeholder='🖊️ Add item...'
+          placeholder='add item..'
+          onChange={(e) => {
+            settodo(e.target.value)
+            console.log(todo)
+          }}
         />
 
         <i
-          onClick={() => settoDOs([...toDOs, { text: toDO, status: false }])}
-          className='fas fa-plus'
+          style={{ ' margin-left': '6px' }}
+          class='fas fa-plus'
+          onClick={() => {
+            setTodo([...Todo, { key: Date.now(), text: todo, status: false }])
+          }}
         ></i>
-      </div>
-      {toDOs.map((each) => {
-        return (
-          <div className='todos'>
-            <div className='todo'>
-              <div className='left'>
-                <input
-                  onChange={(e) => {
-                    // console.log(e.target.c hecked)
-                    settoDOs(
-                      toDOs.filter((nsew) => {
-                        nsew.status = e.target.checked
-                        return nsew
-                      })
-                    )
-                  }}
-                  type='checkbox'
-                  name=''
-                  id=''
-                />
-                <p>{each.text}</p>
-                {console.log(each)};
-              </div>
-              <div className='right'>
-                <i className='fas fa-times'></i>
-              </div>
+
+        {Todo.map((obj) => {
+          console.log(obj)
+          return (
+            <div>
+              
+                <p>{obj.text}</p>
+             
+              <i class='fas fa-times'></i>
             </div>
-          </div>
-        )
-      })}
-      <h1>Completed</h1>
-      {toDOs.map((each) => {
-        if (each.status) {
-          return <h1>{each.text}</h1>
-        }
-        return null
-      })}
+          )
+        })}
+
+
+      </div>
     </div>
   )
 }
-
 export default App
